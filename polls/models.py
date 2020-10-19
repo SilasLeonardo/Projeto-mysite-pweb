0d8723686a0c229fc9ec702ef4e9627378722632
Silas Leonardo <silas.leonardo15@gmail.com>
2020-10-19 12:05:19 -0300

    Add a parte 2

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,11 @@
 from datetime import timedelta
-
 from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
-    question_text = models.CharField(verbose_name='Texto da Questão',max_length=200)
-    pub_date = models.DateTimeField('Data da Publicação')
+    question_text = models.CharField(verbose_name='Texto da Questão',max_length=200,
+                                     help_text="Informe o texto da Questão")
+    pub_date = models.DateTimeField('Data da Publicação', default=timezone.now)
 
     def was_published_recently(self):
         ontem = timezone.now() - timedelta(days=1)
@@ -14,10 +14,20 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @property
+    def maior_choice(self):
+        maior = self.choice_set.aggregate(models.Max('votes'))['votes__max']
+        return self.choice_set.get(votes__exact=maior)
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Questão')
-    choice_text = models.CharField(verbose_name='Texto da Escolha', max_length=200)
+    choice_text = models.CharField('Texto da Escolha', max_length=200)
     votes = models.IntegerField('Numero de Votos',  default=0)
+    pub_date = models.DateTimeField('Data de publicacao', default=timezone.now)
+
+    def was_published_recently_Choice(self):
+        ontem = timezone.now() - timedelta(days=1)
+        return self.pub_date >= ontem
 
     def __str__(self):
         return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,31 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from polls.models import Question
 
-def index(request):
-    print("Minha primira view!")
-    return HttpResponse("Hello, World!")
+def index(request, template_name='polls/index.html'):
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, template_name, context)
+
+
+def detail(request, question_id, template_name='polls/detail.html'):
+    question = get_object_or_404(Question, id=question_id)
+    context = {
+        "question": question,
+    }
+    return render(request, template_name, context)
+
+
+def mvotos(request, question_id, template_name='polls/mvotos.html'):
+    question = get_object_or_404(Question, id=question_id)
+    context = {
+        "question": question,
+    }
+    return render(request, template_name, context)
+
+def sobre(request):
+    print('sobre!')
+    return HttpResponse('<br><b>Equipe:</b><br> <b>1:</b>Silas Leonardo<br><b>2:</b> Deus')
