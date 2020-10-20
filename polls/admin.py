19d208513b44021d0be977f5b91825e197884530
Silas Leonardo <silas.leonardo15@gmail.com>
2020-10-20 15:58:07 -0300

    Parte 5 Concluida

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,58 @@
 from django.contrib import admin
+from django.utils import timezone
 from polls.models import Question, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+    readonly_fields = ('votes', )
+
 class QuestionAdmin(admin.ModelAdmin):
+   list_display = ('question_text', 'pub_date', 'was_published_recently', 'chices_count', 'is_public',)
+   search_fields = ('question_text',)
+   list_filter = ('pub_date', )
+   readonly_fields = ('was_published_recently', 'maior_choice', 'is_public', )
+
+   fieldsets = (
+       ('Dados da questão', {'fields': ('question_text', )}),
+       ('Informações', {'fields': ('pub_date', 'was_published_recently', 'is_public',)}),
+   )
+
+   inlines = (ChoiceInline, )
+   actions = ('action_pub_date_now', 'public_question', 'unpublish', )
+
+   def chices_count(self, obj):
+        return obj.choice_set.count()
+   chices_count.short_description = "Número de Escolhas"
+
+
+   def action_pub_date_now(self, request, queryset):
+       for question in queryset:
+           question.pub_date = timezone.now()
+           question.save()
+   action_pub_date_now.short_description = "Publicar Agora"
+
+
+   def public_question(self, request, queryset):
+       for question in queryset:
+           question.is_public = True
+           question.save()
+
+   public_question.short_description = "Publicar a Question"
+
+
+   def unpublish(self, request, queryset):
+       for question in queryset:
+           question.is_public = False
+           question.save()
+
+   unpublish.short_description = "Despublica a Question"
+
+
+
+
+"""
     fields = ('question_text', 'pub_date', 'maior_choice',)
     readonly_fields = ('maior_choice',)
-
-admin.site.register(Question)
-admin.site.register(Choice)
+"""
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -3,13 +3,22 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
+    class Meta:
+        verbose_name = "Questão"
+        verbose_name_plural = "Questões"
+        ordering = ('pub_date', )
+
     question_text = models.CharField(verbose_name='Texto da Questão',max_length=200,
                                      help_text="Informe o texto da Questão")
     pub_date = models.DateTimeField('Data da Publicação', default=timezone.now)
+    is_public = models.BooleanField('Publicados', default=True)
 
     def was_published_recently(self):
         ontem = timezone.now() - timedelta(days=1)
         return self.pub_date >= ontem
+
+    was_published_recently.short_description = "Publicado Recentemente"
+    was_published_recently.boolean = True
 
     def __str__(self):
         return self.question_text
@@ -20,6 +29,10 @@ class Question(models.Model):
         return self.choice_set.get(votes__exact=maior)
 
 class Choice(models.Model):
+    class Meta:
+        verbose_name = "Opção"
+        verbose_name_plural = "Opções"
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Questão')
     choice_text = models.CharField('Texto da Escolha', max_length=200)
     votes = models.IntegerField('Numero de Votos',  default=0)
