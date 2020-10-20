5fe161eb49967d34a3bcecc7a0b0b136b0befdef
Silas Leonardo <silas.leonardo15@gmail.com>
2020-10-20 03:49:07 -0300

    Parte 4 Concluida

--- polls/views.py
+++ polls/views.py
@@ -1,31 +1,53 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
 from polls.models import Question
 
-def index(request, template_name="polls/index.html"):
-    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, template_name, context )
+class IndexView(generic.ListView):
+    template_name= "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.all().order_by("-pub_date")[:5]
 
 
-def detail(request, question_id, template_name='polls/detail.html'):
-    question = get_object_or_404(Question, id=question_id)
-    context = {
-        "question": question,
-    }
-    return render(request, template_name, context)
+class DetailView(generic.DetailView):
+    template_name = 'polls/detail.html'
+    model = Question
+
+class VoteView(generic.View):
+    def post(self, request, question_id):
+        question = get_object_or_404(Question, id=question_id)
+        choice = question.choice_set.get(id=request.POST['choice'])
+        choice.votes += 1
+        choice.save()
+        return redirect('detail', question_id)
 
 
-def mvotos(request, question_id, template_name='polls/mvotos.html'):
-    question = get_object_or_404(Question, id=question_id)
-    context = {
-        "question": question,
-    }
-    return render(request, template_name, context)
+class MvotosView(generic.DetailView):
+    template_name = 'polls/mvotos.html'
+    model = Question
 
+class MMvotosView(generic.View):
+
+    def post(self, request, question_id):
+
+        question = get_object_or_404(Question, id=question_id)
+        #print(request.POST)
+        choice = question.choice_set.get(id=request.POST['choice'])
+        choice.votes+= 1
+        choice.save()
+        return redirect('mvotos', question_id)
+
+
+class SobreView(generic.TemplateView):
+    template_name = 'polls/sobre.html'
+
+    def get_queryset(self):
+        return redirect('sobre')
+"""
 def sobre(request):
     print('sobre!')
     return HttpResponse('<br><b>Equipe:</b><br> <b>1:</b>Silas Leonardo<br><b>')
+"""
